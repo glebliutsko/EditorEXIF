907122a1ed445bc8b01a485045d38e6c698856d0
Gleb Liutsko <gleb@liutsko.ru>
2021-10-26 21:23:01 +0300

    Initial commit

---
+++ PhotoDateEditor/Image/ImageMetadata.cs
@@ -0,0 +1,41 @@
+﻿using ExifLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoDateEditor.Image
+{
+    class ImageMetadata
+    {
+        public ImageFile Metadata { get; private set; }
+        public string PathToFile { get; private set; }
+
+        public string FileName
+        {
+            get
+            {
+                return Path.GetFileName(PathToFile);
+            }
+        }
+
+        public DateTime CreateDateTime
+        {
+            get
+            {
+                return Metadata.Properties.Get<ExifDateTime>(ExifTag.DateTimeOriginal);
+            }
+        }
+
+
+        public ImageMetadata(string pathToFile)
+        {
+            Metadata = ImageFile.FromFile(pathToFile);
+            PathToFile = pathToFile;
+        }
+
+        
+    }
+}
---
+++ PhotoDateEditor/Windows/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using PhotoDateEditor.Image;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace PhotoDateEditor.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<ImageMetadata> Images { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Images = new();
+            PhotosListBox.ItemsSource = Images;
+        }
+
+        private void PhotosListBox_Drop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetFormats().Contains(DataFormats.FileDrop))
+                return;
+
+            var fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            var images = fileNames.Select(x => new ImageMetadata(x)).ToArray();
+            foreach (var i in images)
+                Images.Add(i);
+        }
+    }
+}
